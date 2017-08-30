c643496a95f9823a8bbe7f67926340947568ca10
nelmandela <nelsonmandela784@gmail.com>
2017-08-30 07:03:29 +0300

    Modify manage file

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
 import os
+import unittest
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from app import db, create_app
@@ -9,6 +10,15 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
+
+@manager.command
+def test():
+    """Runs the unit tests without test coverage."""
+    tests = unittest.TestLoader().discover('./tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
 
 if __name__ == '__main__':
     manager.run()
